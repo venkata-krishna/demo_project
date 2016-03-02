21274857a8bd8decb9679c05888342bc230a8bf9
venkata-krishna <venkata.krishna@gladminds.co>
2016-03-02 15:40:22 +0530

    2/3/16 get t the details

--- demo/urls.py
+++ demo/urls.py
@@ -8,9 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'demo.views.home', name='home'),t
     # url(r'^blog/', include('blog.urls')),
-    url('^login/',include('login.urls')),
-    url(r'^cost/$',views.costView, name="cost"),
+    url(r'^login/',include('login.urls')),
     url(r'^cost/',include('demo_strbd.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 )
